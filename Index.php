23d213a6595c1d4cb3df6644e823dba0a88b532f
benjaminekill <benjaminekill@hotmail.com>
2014-10-17 23:51:28 -0700

    Revert "Revert "InputBootsatrap and test""
    This reverts commit f3b9413a4fd1d241217ebb1a962249134841c6fa.

---
+++ Index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+<meta charset="UTF-8">
+<title>Guay Tiao Kang Tang</title>
+<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+<style>
+
+		/* Styles that are also copied for Preview */
+		body {
+			margin: 10px 0 0 10px;
+		}
+		
+		.control-label {
+			display: inline-block !important;
+			pasdding-top: 5px;
+			text-align: right;
+			vertical-align: baseline;
+			padding-right: 10px;
+		}
+		
+		.droppedField {
+			padding-left:5px;
+		}
+
+		.droppedField > input,select, button, .checkboxgroup, .selectmultiple, .radiogroup {
+			margin-top: 10px;
+			
+			margin-right: 10px;
+			margin-bottom: 10px;
+		}
+
+		.action-bar .droppedField {
+			float: left;
+			padding-left:5px;
+		}
+
+	
+</style>
+</head>
+<body><legend>Guay Tiao Kang Tang Se</legend>
+		<table border="2" width"800">
+		<th>
+		<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Form Name</legend>
+<img src="Img\Image.jpg" alt="..." class="img-rounded">
+</fieldset>
+</form>
+		
+		
+		
+		</th>
+		<th> 
+		
+		
+		
+<form class="form-horizontal">
+<fieldset>
+
+<!-- Form Name -->
+<legend>Guay Tiao Kang Tang </legend>
+
+<!-- Multiple Radios -->
+<div class="control-group">
+  <label class="control-label" for="radios">Multiple Radios</label>
+  <div class="controls">
+    <label class="radio" for="radios-0">
+      <input type="radio" name="radios" id="radios-0" value="Option one" checked="checked">
+      Option one
+    </label>
+    <label class="radio" for="radios-1">
+      <input type="radio" name="radios" id="radios-1" value="Option two">
+      Option two
+    </label>
+  </div>
+</div>
+
+</fieldset>
+</form>
+		
+  
+  </div>
+ 
+</div>
+
+
+
+
+		</th>
+		</table>
+	  
+	  
+
+
+	  
+	
+</body></html>
